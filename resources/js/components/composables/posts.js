3c433d33ce9560a0547b1be042749ff078d0591b
Mark Railton <marksrailton@gmail.com>
2022-05-24 21:34:39 +0100

    Adds ability to save a new post

--- resources/js/components/composables/posts.js
+++ resources/js/components/composables/posts.js
@@ -1,14 +1,23 @@
 import {ref} from 'vue';
+import {useRouter} from 'vue-router';
 
 export default function usePosts() {
     const posts = ref({});
+    const router = useRouter();
 
     const getPosts = async (page = 1, category = '', orderColumn = 'created_at', orderDirection = 'desc') => {
         axios.get('/api/posts?page=' + page + '&category=' + category + '&order_column=' + orderColumn + '&order_direction=' + orderDirection)
             .then(response => {
                 posts.value = response.data;
             });
-    }
+    };
 
-    return {posts, getPosts};
+    const storePost = async (post) => {
+        axios.post('/api/posts', post)
+            .then(response => {
+                router.push({name: 'posts.index'});
+            });
+    };
+
+    return {posts, getPosts, storePost};
 }
