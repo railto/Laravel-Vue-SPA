98276947e4d0b01e40e0c82e11805e333d2e9ce5
Mark Railton <marksrailton@gmail.com>
2022-05-24 21:04:27 +0100

    Setting up router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,23 @@ require('./bootstrap');
 
 import {createApp} from 'vue';
 import LaravelVuePagination from "laravel-vue-pagination";
-import PostsIndex from './components/Posts/Index';
+import {createRouter, createWebHistory} from "vue-router";
 
-const app = createApp({});
-app.component('posts-index', PostsIndex);
+import App from './layouts/App'
+import PostsIndex from './components/Posts/Index';
+import PostsCreate from './components/Posts/Create';
+
+const routes = [
+    {path: '/', component: PostsIndex},
+    {path: '/posts/create', component: PostsCreate},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
 app.component('Pagination', LaravelVuePagination);
 app.mount('#app');
