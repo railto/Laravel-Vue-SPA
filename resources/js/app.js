18b0b25b83aacc70ec6b387a2dac032ef31a4ee3
Mark Railton <marksrailton@gmail.com>
2022-05-24 21:14:45 +0100

    Split router to its own file

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,23 +2,12 @@ require('./bootstrap');
 
 import {createApp} from 'vue';
 import LaravelVuePagination from "laravel-vue-pagination";
-import {createRouter, createWebHistory} from "vue-router";
 
 import App from './layouts/App'
-import PostsIndex from './components/Posts/Index';
-import PostsCreate from './components/Posts/Create';
-
-const routes = [
-    {path: '/', component: PostsIndex},
-    {path: '/posts/create', component: PostsCreate},
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+import router from './routes/index';
 
 const app = createApp(App);
+
 app.use(router);
 app.component('Pagination', LaravelVuePagination);
 app.mount('#app');
