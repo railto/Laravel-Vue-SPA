5b8f7e6ab09918260c32945d9139a33e06356883
Mark Railton <marksrailton@gmail.com>
2022-05-24 23:24:54 +0100

    getting auth up and running

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,9 @@ import {createApp} from 'vue';
 import LaravelVuePagination from "laravel-vue-pagination";
 import VueSweetalert2 from "vue-sweetalert2";
 
-import App from './layouts/App'
 import router from './routes/index';
 
-const app = createApp(App);
+const app = createApp({});
 
 app.use(router);
 app.use(VueSweetalert2);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::view('/{any?}', 'dashboard')
     ->name('dashboard')
     ->where('any', '.*');
+
+Route::post('login',[AuthenticatedSessionController::class, 'store']);
